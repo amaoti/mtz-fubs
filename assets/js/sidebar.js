9796d4cb7b9d18ab8b483f572214893b10fe1349
amaoti <aoti.work@gmail.com>
2025-03-10 19:38:22 -0400

    -

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,26 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let sidebar = document.querySelector(".sidebar");
+    let sidebar = document.getElementById("sidebar");
     let sidebarToggle = document.querySelector(".sidebar-toggle");
-    let sidebarLinks = document.querySelectorAll(".sidebar a");
 
     function toggleSidebar() {
         sidebar.classList.toggle("active");
-        sidebarToggle.classList.toggle("hidden"); 
+        sidebarToggle.textContent = sidebar.classList.contains("active") ? "✖ Close" : "☰ Stories";
     }
 
     sidebarToggle.addEventListener("click", toggleSidebar);
-
-    sidebarLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            sidebar.classList.remove("active");
-            sidebarToggle.classList.remove("hidden"); 
-        });
-    });
-
-    document.addEventListener("click", function (event) {
-        if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
-            sidebar.classList.remove("active");
-            sidebarToggle.classList.remove("hidden");
-        }
-    });
 });
