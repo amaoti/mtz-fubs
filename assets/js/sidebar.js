4fd99778b42b76c82356a3dfbe45488cb288e4fc
amaoti <aoti.work@gmail.com>
2025-03-10 19:26:59 -0400

    -

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,17 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     let sidebar = document.querySelector(".sidebar");
+    let sidebarToggle = document.querySelector(".sidebar-toggle");
     let startOffset = sidebar.offsetTop;
-    let marginTop = 20; // Space from the top when scrolling
+    let marginTop = 20;
+    let isSidebarOpen = false;
+
+    function toggleSidebar() {
+        sidebar.classList.toggle("active");
+        isSidebarOpen = !isSidebarOpen;
+    }
+
+    sidebarToggle.addEventListener("click", toggleSidebar);
 
     window.addEventListener("scroll", function () {
         let scrollTop = window.scrollY;
 
-        if (scrollTop > startOffset - marginTop) {
-            sidebar.style.position = "fixed";
-            sidebar.style.top = marginTop + "px";
-        } else {
-            sidebar.style.position = "absolute";
-            sidebar.style.top = startOffset + "px";
+        if (!isSidebarOpen) {
+            if (scrollTop > startOffset - marginTop) {
+                sidebar.style.position = "fixed";
+                sidebar.style.top = marginTop + "px";
+            } else {
+                sidebar.style.position = "absolute";
+                sidebar.style.top = startOffset + "px";
+            }
         }
     });
 });
