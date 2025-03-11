c596b836b37ac9402ca26e936a20637c31340d4b
amaoti <aoti.work@gmail.com>
2025-03-10 20:37:13 -0400

    -

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function toggleSidebar() {
         sidebar.classList.toggle("active");
         if (sidebar.classList.contains("active")) {
-            sidebarToggle.textContent = "✖ Stories";
+            sidebarToggle.textContent = "🗙 Stories";
         } else {
             sidebarToggle.textContent = "☰ Stories";
         }
